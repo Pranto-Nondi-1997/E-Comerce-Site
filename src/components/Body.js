b3f8cd535546133fd2f687665dc6c9a758ec13bb
Pranto <pranto.cse.bd@gmail.com>
2021-09-29 02:21:26 +0600

    progress

--- src/components/Body.js
+++ src/components/Body.js
@@ -33,39 +33,39 @@ const Body = () => {
     }
 
     return (
-        <div class="">
+        <div className="">
             <br />
-            <div class="row">
-                <div class="col-8">
-                    <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div className="row">
+                <div className="col-8">
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
                         {products.map(product => {
-                            return <div class="col">
+                            return <div className="col">
                                 <div class="card h-100">
-                                    <img src={product.img} class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h4 class="card-title">Set Name:{product.phone}</h4>
-                                        <h5 class="card-title">Price:{product.price}</h5>
-                                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <button type="button" class="btn btn-danger" onClick={() => addToHandel(product)} >Puchase Now</button>
+                                    <img src={product.img} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h4 className="card-title">Set Name:{product.phone}</h4>
+                                        <h5 className="card-title">Price:{product.price}</h5>
+                                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <button type="button" className="btn btn-danger" onClick={() => addToHandel(product)} >Puchase Now</button>
                                     </div>
                                 </div>
                             </div>
                         })}
                     </div>
                 </div>
-                <div class="col-4">
-                    <div class="card text-center">
-                        <div class="card-header">
+                <div className="col-4">
+                    <div className="card text-center">
+                        <div className="card-header">
                             Product Total History
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Item:{card.length}</h5>
-                            <p class="card-text" >Price:<span id="price">0</span></p>
-                            <p class="card-text">Product List<ol>
+                        <div className="card-body">
+                            <h6 className="card-title">Item:{card.length}</h6>
+                            <h5 className="card-text" >Price:<span id="price">0</span></h5>
+                            <p className="card-text">Product List<ol>
                                 <span id="productName"></span></ol></p>
-                            <a href class="btn btn-primary">Details</a>
+                            <a href className="btn btn-primary">Details</a>
                         </div>
-                        <div class="card-footer text-muted">
+                        <div className="card-footer text-muted">
 
                         </div>
                     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,10 @@ let Card = (props) => {
         <div className="">
             <div class="card" >
                 <img src={img} class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
 
