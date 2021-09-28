57795e3dfd5b46aedf4844ec8e9a4d35f151515c
Pranto <pranto.cse.bd@gmail.com>
2021-09-29 01:54:08 +0600

    progress

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import './Body.css'
-
 const Body = () => {
     let [products, setProducts] = useState([]);
+    let [card, setCard] = useState([]);
     useEffect(() => {
         fetch('./manifest.json')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+    let addToHandel = (product) => {
+
+        let newCard = [...card, product];
+
+        setCard(newCard);
+        // console.log(card);
+        // console.log(newCard);
+        callPrice(newCard);
+    }
+    let callPrice = (newProducts) => {
+
+        let total = 0;
+        let setName = document.getElementById('productName').innerText;
+        newProducts.map(newProduct => {
+            total = total + newProduct.price;
+            document.getElementById('price').innerText = total;
+
+            document.getElementById('productName').innerText = (newProduct.phone + '+' + setName);
+
+        })
+
+    }
 
     return (
-
-
         <div class="">
             <br />
             <div class="row">
@@ -26,27 +46,27 @@ const Body = () => {
                                         <h4 class="card-title">Set Name:{product.phone}</h4>
                                         <h5 class="card-title">Price:{product.price}</h5>
                                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                        <button type="button" onClick class="btn btn-danger">Puchase Now</button>
+                                        <button type="button" class="btn btn-danger" onClick={() => addToHandel(product)} >Puchase Now</button>
                                     </div>
                                 </div>
                             </div>
                         })}
-
-
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="card text-center">
                         <div class="card-header">
-                            Featured
+                            Product Total History
                         </div>
                         <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">Item:{card.length}</h5>
+                            <p class="card-text" >Price:<span id="price">0</span></p>
+                            <p class="card-text">Product List<ol>
+                                <span id="productName"></span></ol></p>
+                            <a href class="btn btn-primary">Details</a>
                         </div>
                         <div class="card-footer text-muted">
-                            2 days ago
+
                         </div>
                     </div>
                 </div>
